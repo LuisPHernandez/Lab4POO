18e6481c529f2d1b3403bc0f301c49d32e008bb1
Luis Pedro Hernandez <lphm0105@gmail.com>
2024-11-11 12:57:29 -0600

    Agregar atributos

---
+++ Carro.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+public class Carro implements BMWClaseA {
+    //Atributos
+    private boolean encendido;
+    private int temperaturaInt;
+    private boolean modoAutomatico;
+    private int temperaturaExt;
+    private int nivelVentilacion;
+    private String direccionVentilacion;
+    private boolean circulacionInterna;
+    private int nivelCalefaccionAsientoConductor;
+    private int nivelCalefaccionAsientoPasajero;
+    private int nivelCalefafaccionaAsientosTraseros;
+    private String nivelCalefaccionVolante;
+    private boolean calefaccionRapida;
+    private boolean desempañador;
+    private String ionizador;
+    private String calidadDeAire;
+    private boolean purificacionProfunda;
+    private int ionizadorAutolimpieza;
+    private boolean ionizadorBajoConsumo;
+    private ArrayList<Object> perfil1;
+    private ArrayList<Object> perfil2;
+    private ArrayList<Object> perfil3;
+    private ArrayList<String> historialMantenimienot;
+    private String fechaDeMantenimiento;
+
+    //Métodos
+    public Carro(){
+
+    }
+}
