ba69f90ab98565f492eb6684605d1dcd8256ef89
Luis Pedro Hernandez <lphm0105@gmail.com>
2024-11-13 22:45:19 -0600

    Agregar comentarios

--- ClaseA.java
+++ ClaseA.java
@@ -4,25 +4,85 @@ import java.util.Date;
 
 public class ClaseA implements BMWClaseA {
     //Atributos
+    /**
+     *
+     */
     private boolean encendido;
+    /**
+     *
+     */
     private int temperaturaInt;
+    /**
+     *
+     */
     private int temperaturaExt;
+    /**
+     *
+     */
     private int nivelVentilacion;
+    /**
+     *
+     */
     private String direccionVentilacion;
+    /**
+     *
+     */
     private boolean circulacionInterna;
+    /**
+     *
+     */
     private int nivelCalefaccionAsientos;
+    /**
+     *
+     */
     private String nivelCalefaccionVolante;
+    /**
+     *
+     */
     private boolean calefaccionRapida;
+    /**
+     *
+     */
     private boolean desempañador;
+    /**
+     *
+     */
     private boolean ionizador;
+    /**
+     *
+     */
     private String intensidadIonizador;
+    /**
+     *
+     */
     private String calidadDeAire;
+    /**
+     *
+     */
     private int ionizadorAutolimpieza;
+    /**
+     *
+     */
     private int numeroPerfiles;
+    /**
+     *
+     */
     private ArrayList<String> perfil1;
+    /**
+     *
+     */
     private ArrayList<String> perfil2;
+    /**
+     *
+     */
     private ArrayList<String> perfil3;
+    /**
+     *
+     */
     private ArrayList<String> historialMantenimiento;
+    /**
+     *
+     */
     private String fechaDeMantenimiento;
 
     //Métodos
@@ -53,56 +113,66 @@ public class ClaseA implements BMWClaseA {
         this.fechaDeMantenimiento = "";
     }
 
+    //Cambia el estado del sistema a encendid0
     public void encender(){
         this.encendido = true;
     }
 
+    //Cambia el estado del sistema a apagado
     public void apagar(){
         this.encendido = false;
     }
 
+    //Ajusta la temperatura interior del vehículo por el incremento que el usuario ingrese
     public void ajustarTemperatura(int incremento){
         if (encendido) {
             this.temperaturaInt += incremento;
         }
     }
 
+    //Activa el modo de operación automático del vehículo, que establece la temperatura interior al nivel de la externa
     public void activarModoAutomatico(){
         if (encendido) {
             this.temperaturaInt = this.temperaturaExt;
         } 
     }
 
+    //Ajusta el nivel de ventilación del vehículo por el nivel que el usuario ingrese
     public void ajustarNivelVentilacion(int nivel){
         if (encendido) {
             this.nivelVentilacion = nivel;
         } 
     }
 
+    //Cambia la dirección del flujo de aire del vehículo por la dirección que el usuario ingrese
     public void cambiarDireccionFlujo(String direccion){
         if (encendido) {
             this.direccionVentilacion = direccion;
         } 
     }
 
+    //Activa circulación del aire solo adentro del vehículo
     public void activarCirculacionInterna(){
         if (encendido) {
             this.circulacionInterna = !this.circulacionInterna;
         } 
     }
 
+    //Ajusta el nivel de calefacción del asiento por el nivel que el usuario ingrese
     public void ajustarCalefaccionAsientos(int nivel){
         if (encendido) {
             this.nivelCalefaccionAsientos = nivel;
         }
     }
 
+    //Activa la calefacción del volante con el nivel que el usuario ingrese
     public void activarCalefaccionVolante(String nivel){
         if (encendido) {
             this.nivelCalefaccionVolante = nivel;
         }
     }
 
+    //Activa la calefacción rápida del vehículo, que aumenta el nivel de calefacción del asiento y del volante
     public void activarCalefaccionRapida() {
         if (encendido) {
             if (this.calefaccionRapida == false) {
@@ -116,18 +186,21 @@ public class ClaseA implements BMWClaseA {
         }
     }
 
+    //Activa y desactiva el desempañador
     public void activarDesempañador() {
         if (encendido) {
             this.desempañador = !this.desempañador;
         }
     }
 
+    //Activa y desactiva el ionizador
     public void activarIonizador(){
         if (encendido) {
             this.ionizador = !this.ionizador;
         }
     }
 
+    //Activa el ionizador automáticamente  si la calidad del aire es mala según el nivel que el usuario ingrese
     public void autoIonizador(String nivel){
         if (encendido) {
             if (this.ionizador == false) {
@@ -139,12 +212,14 @@ public class ClaseA implements BMWClaseA {
         }
     }
 
+    //Ajusta la intensidad del ionizador por el nivel que el usuario ingrese
     public void ajustarIonizador(String nivel){
         if (encendido && this.ionizador) {
             this.intensidadIonizador = nivel;
         }
     }
 
+    //Activa el ciclo de purificación profunda del ionizador
     public String cicloIonizador() {
         if (encendido) {
             if (this.ionizador == false) {
@@ -159,18 +234,21 @@ public class ClaseA implements BMWClaseA {
         return "El carro está apagado.";
     }
 
+    //Activa la autolimpieza del ionizador
     public void autoLimpiezaIonizador() {
         if (encendido) {
             this.ionizadorAutolimpieza += 1;
         }
     }
 
+    //Pone el ionizador en intensidad baja para guardar energía
     public void reduceIonizador() {
         if (encendido) {
             this.intensidadIonizador = "bajo";
         }
     }
 
+    //Crea un perfil con los valores actuales
     public void crearPerfil(String nombre) {
         if (encendido){
             ArrayList<String> perfil = new ArrayList<>();
@@ -198,6 +276,7 @@ public class ClaseA implements BMWClaseA {
         }   
     }
 
+    //Permite elegir un perfil con valores almacenados
     public void elegirPerfil(String nombre) {
         if (encendido) {
             ArrayList<String> perfil = null;
@@ -225,6 +304,7 @@ public class ClaseA implements BMWClaseA {
         } 
     }
 
+    //Determina si es necesario un mantenimiento, si lo es lo realiza
     public boolean mantenimiento() {
         Random random = new Random();
         boolean mantenimiento = random.nextBoolean();
@@ -236,10 +316,12 @@ public class ClaseA implements BMWClaseA {
         return mantenimiento;
     }
 
+    //Muestra el historial de mantenimientos realizados
     public String historialMantenimiento() {
         return String.join(", ", historialMantenimiento);
     }
 
+    //Programa una fecha de mantenimiento para el sistema
     public void programarMantenimiento(Date fecha) {
         this.fechaDeMantenimiento = fecha.toString();
     }
